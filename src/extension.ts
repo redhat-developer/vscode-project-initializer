970bfe8b0ba8e40943702cafcd959223892fc0e7
Jeff MAURY <jmaury@redhat.com>
2018-11-20 09:54:02 +0100

    Renamed to Project Initializer
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import * as yauzl from 'yauzl'
 let fs = require('fs');
 let p = require('path');
 
-let catalogBuilder:Catalog = new Catalog(vscode.workspace.getConfiguration("fabric8.launcher").get<string>("endpointUrl", "https://forge.api.openshift.io/api/"));
+let catalogBuilder:Catalog = new Catalog(vscode.workspace.getConfiguration("project.initializer").get<string>("endpointUrl", "https://forge.api.openshift.io/api/"));
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposableCommand = vscode.commands.registerCommand('fabric8.launcher.generate', () => generate());
+    let disposableCommand = vscode.commands.registerCommand('project.initializer.generate', () => generate());
 
     let disposableListener = vscode.workspace.onDidChangeConfiguration(event => {updateCatalog(event)});
 
@@ -24,8 +24,8 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function updateCatalog(event: vscode.ConfigurationChangeEvent) {
-    if (event.affectsConfiguration("fabric8.launcher.endpointUrl")) {
-        let url = vscode.workspace.getConfiguration("fabric8.launcher").get<string>("endpointUrl", "https://forge.api.openshift.io/api/");
+    if (event.affectsConfiguration("project.initializer.endpointUrl")) {
+        let url = vscode.workspace.getConfiguration("project.initializer").get<string>("endpointUrl", "https://forge.api.openshift.io/api/");
         if (catalogBuilder.endpoint != url) {
             catalogBuilder = new Catalog(url);
         }
@@ -42,11 +42,11 @@ async function generate() {
             if (boosters) {
                 let runtimeId:any = await vscode.window.showQuickPick(boosters.map((item:any) => ({label:item.runtime, description:item.version})),{placeHolder: 'Choose runtime'});
                 if (runtimeId) {
-                    let groupId = await vscode.window.showInputBox({prompt:'Group Id', placeHolder:'Enter the group id', value: vscode.workspace.getConfiguration("fabric8.launcher").get<string>("defaultGroupId")});
+                    let groupId = await vscode.window.showInputBox({prompt:'Group Id', placeHolder:'Enter the group id', value: vscode.workspace.getConfiguration("project.initializer").get<string>("defaultGroupId")});
                     if (groupId) {
-                        let artifactId = await vscode.window.showInputBox({prompt:'Artifact Id', placeHolder:'Enter the artifact id', value: vscode.workspace.getConfiguration("fabric8.launcher").get<string>("defaultArtifactId")});
+                        let artifactId = await vscode.window.showInputBox({prompt:'Artifact Id', placeHolder:'Enter the artifact id', value: vscode.workspace.getConfiguration("project.initializer").get<string>("defaultArtifactId")});
                         if (artifactId) {
-                            let version = await vscode.window.showInputBox({prompt:'Version',placeHolder:'Enter the version', value: vscode.workspace.getConfiguration("fabric8.launcher").get<string>("defaultVersion")});
+                            let version = await vscode.window.showInputBox({prompt:'Version',placeHolder:'Enter the version', value: vscode.workspace.getConfiguration("project.initializer").get<string>("defaultVersion")});
                             if (version) {
                                 vscode.window.setStatusBarMessage("Downloading zip project file", 1000);
                                 let zipProject = await catalogBuilder.zip(artifactId, missionId.label, runtimeId.label, runtimeId.description, groupId, artifactId, version);
@@ -66,7 +66,7 @@ async function generate() {
         }
     }
     catch (error) {
-        vscode.window.showErrorMessage("Error while processing Fabric8 launcher" + error);
+        vscode.window.showErrorMessage("Error while processing Project Initializer" + error);
     }
     
 }
