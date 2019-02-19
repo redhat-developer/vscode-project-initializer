dd148934c89f0e19501ee2dc0777a6794ed35eaa
Aurélien Pupier <apupier@redhat.com>
2019-02-19 16:14:27 +0100

    Provide a specific Camel/Fuse command #21
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -17,32 +17,31 @@ import { Catalog } from '../Catalog';
 suite("Extension Tests", function () {
 
     test('Extension should be present', () => {
-		assert.ok(vscode.extensions.getExtension('redhat.project-initializer'));
-	});
+        assert.ok(vscode.extensions.getExtension('redhat.project-initializer'));
+    });
 
-	test('should activate', function () {
-		this.timeout(1 * 60 * 1000);
-		return vscode.extensions.getExtension('redhat.project-initializer')!.activate().then((api) => {
-			assert.ok(true);
-		});
-	});
+    test('should activate', function () {
+        this.timeout(1 * 60 * 1000);
+        return vscode.extensions.getExtension('redhat.project-initializer')!.activate().then((api) => {
+            assert.ok(true);
+        });
+    });
 
-	test('should register all commands', function () {
-		return vscode.commands.getCommands(true).then((commands) =>
-		{
-			let myCommands = commands.filter(function(value){
-				return value.startsWith('project.initializer');
-			});
-			assert.equal(myCommands.length, 1, 'Some commands are not registered properly or a new command is not added to the test');
-		});
-	});
+    test('should register all commands', function () {
+        return vscode.commands.getCommands(true).then((commands) =>
+        {
+            let myCommands = commands.filter(function(value){
+                return value.startsWith('project.initializer');
+            });
+            assert.equal(myCommands.length, 2, 'Some commands are not registered properly or a new command is not added to the test');
+        });
+    });
 
-	test('Should load default catalog', function() {
-		let catalog = new Catalog('https://forge.api.openshift.io/api/');
-		return catalog.getCatalog().then(res => {
-			assert.ok(res);
-		})
+    test('Should load default catalog', function() {
+        let catalog = new Catalog('https://forge.api.openshift.io/api/');
+        return catalog.getCatalog().then(res => {
+            assert.ok(res);
+        });
 
-	});
-
+    });
 });
