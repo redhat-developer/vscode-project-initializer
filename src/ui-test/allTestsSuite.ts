c758777375a8c048ff4c652a304aa64e5f0bfa52
Ondrej Dockal <odockal@redhat.com>
2020-02-24 08:17:57 +0100

    Add license file EPL 2.0     - add full test suite
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- src/ui-test/allTestsSuite.ts
+++ src/ui-test/allTestsSuite.ts
@@ -1,14 +1,14 @@
-// import { baseExtensionUITest } from "./baseExtensionTest";
-// import { testCommandPaletteOffering } from "./commandPaletteOptionsTest";
+import { baseExtensionUITest } from "./baseExtensionTest";
+import { testCommandPaletteOffering } from "./commandPaletteOptionsTest";
 import { testCreatingCamelProject } from "./camelProjectCreateTest";
-// import { testFuseProjectOffering } from "./camelProjectOfferingTest";
+import { testFuseProjectOffering } from "./camelProjectOfferingTest";
 
 /**
  * @author Ondrej Dockal <odockal@redhat.com>
  */
 describe("Project initializer UI tests", () => {
-   // baseExtensionUITest();
-   // testCommandPaletteOffering();
+   baseExtensionUITest();
+   testCommandPaletteOffering();
    testCreatingCamelProject();
-   // testFuseProjectOffering();
+   testFuseProjectOffering();
 });
