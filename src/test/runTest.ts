e3e7cf8063971a175d794f044ae95916ddc899f8
Jeff MAURY <jmaury@redhat.com>
2021-06-15 16:09:22 +0200

    Remove vscode-commons installation from tests
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -17,15 +17,6 @@ async function main() {
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './index');
 
-    const vscodeExecutablePath = await downloadAndUnzipVSCode();
-    const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
-
-    // Use cp.spawn / cp.exec for custom setup
-    cp.spawnSync(cliPath, ['--install-extension', 'redhat.vscode-commons'], {
-      encoding: 'utf-8',
-      stdio: 'inherit'
-    });
-
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
   } catch (err) {
